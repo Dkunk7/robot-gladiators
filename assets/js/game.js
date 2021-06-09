7484a03e4ed268ad344a66ccf84dc5ef00fbf456
Dylan K <drkunkel7@gmail.com>
2021-06-09 16:45:45 -0500

    high scores

--- assets/js/game.js
+++ assets/js/game.js
@@ -166,10 +166,23 @@ var startGame = function() {
 //fucntion to end the game
 var endGame =  function() {
     if (playerInfo.health > 0) {
-        window.alert("Great job, you've survived the game! You now have a score of " + playerInfo.money + ".");
+        window.alert("The game has now ended. Let's see how you did!");
     }
     else {
-        window.alert("You've lost your robot in batter.");
+        window.alert("You've lost your robot in battel.");
+    }
+    var highScore = localStorage.getItem("highscore");
+    if (highScore === null) {
+        highScore = 0;
+    }
+    if (playerInfo.money > highScore) {
+        localStorage.setItem("highscore", playerInfo.money);
+        localStorage.setItem("name", playerInfo.name);
+
+        alert(playerInfo.name + " now has the high score of " + playerInfo.money + "!");
+    }
+    else {
+        alert(playerInfo.name + " did not beat the high score of " + highScore + ". Maybe next time!");
     }
     //ask to play again
     var playAgainConfirm = window.confirm("Would you like to play again?");
@@ -180,7 +193,7 @@ var endGame =  function() {
     else {
         window.alert("Thank you for playing Robot Gladiators! Come back soon!");
     }
-}
+};
 
 var shop = function() {
     var shopOptionPrompt =  window.prompt(
